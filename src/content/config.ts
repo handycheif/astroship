006693182ccfa295b3dbf4ed79436cdb78a1bf9b
handycheif <damianpostigo@gmail.com>
2026-08-02 07:13:27 -0700

    Refactor image and avatar object definitions

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,11 @@
-// 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 
-// 2. Define your collection(s)
 const blogCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
     snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    image: z.object({ src: z.string(), alt: z.string() }),
     publishDate: z.string().transform(str => new Date(str)),
     author: z.string().default('Astroship'),
     category: z.string(),
@@ -23,33 +18,25 @@ const teamCollection = defineCollection({
     draft: z.boolean(),
     name: z.string(),
     title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    avatar: z.object({ src: z.string(), alt: z.string() }),
     publishDate: z.string().transform(str => new Date(str)),
   }),
 });
 
-// NEW: Define your products collection
+// We added this part for your products:
 const productsCollection = defineCollection({
   schema: z.object({
     draft: z.boolean().default(false),
     title: z.string(),
     price: z.number(),
     snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    image: z.object({ src: z.string(), alt: z.string() }),
     paymentLink: z.string(),
   }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
   'team': teamCollection,
-  'products': productsCollection, // NEW: registered products
+  'products': productsCollection, // This registers it
 };
